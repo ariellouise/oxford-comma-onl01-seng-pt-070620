d2d5adc6a5dfb241447592ab913acec1edc0af3a
ariel murphy <arielladders@gmail.com>
2020-07-14 13:50:37 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,9 +6,5 @@ def oxford_comma(array)
   elseif array.length == 2 
     return array.join( " and ")
   elseif array.length >= 3
-    return array.join
-    
-    
-    
-    #array.join (",")
+    return array[0..-2].join(', ') + ", and " + array[-1]
 end
