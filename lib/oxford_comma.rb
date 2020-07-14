af372762794ff666422e8c05b62795d161951dd9
ariel murphy <arielladders@gmail.com>
2020-07-14 13:48:14 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
     return array.join
   elseif array.length == 2 
     return array.join( " and ")
-  elseif array.length  3
+  elseif array.length >= 3
     return array.join
     
     
