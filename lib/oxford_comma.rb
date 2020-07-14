4d236a19ea8cbccd85451c917b9ca22c60dc1311
ariel murphy <arielladders@gmail.com>
2020-07-14 13:47:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,8 @@ array = ["fiddleheads", "okra", "kohlrabi"]
 def oxford_comma(array)
   if array.length == 1
     return array.join
-  elseif array
+  elseif array.length == 2 
+    return array.join("and")
     
     
     
